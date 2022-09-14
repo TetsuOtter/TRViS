82d3391ca3c6f4692c38b509389355efc48d65ab
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-09-14 18:30:46 +0900

    文字列の全角化関数などをUtilsに移動

--- TRViS/Models/TimetableRow.cs
+++ TRViS/Models/TimetableRow.cs
@@ -18,35 +18,13 @@ public record TimetableRow(
 {
 	const string SPACE_CHAR = "\x2002";
 	const string SPACE_CHAR_4 = "\x2002\x2002\x2002\x2002";
+
 	public string SpacedStationName
 		=> StationName.Length switch
 		{
-			2 => ToDispString(StationName, SPACE_CHAR_4),
-			3 => ToDispString(StationName, SPACE_CHAR),
-			4 => ToDispString(StationName, "\x200A"),
+			2 => Utils.InsertCharBetweenCharAndMakeWide(StationName, SPACE_CHAR_4),
+			3 => Utils.InsertCharBetweenCharAndMakeWide(StationName, SPACE_CHAR),
+			4 => Utils.InsertCharBetweenCharAndMakeWide(StationName, "\x200A"),
 			_ => StationName
-		};
-
-	static string ToDispString(string input, string toInsert)
-	{
-		string? ret = null;
-		foreach (char v in input)
-		{
-			char c = ToWide(v);
-
-			if (ret is null)
-				ret = c.ToString();
-			else
-				ret += toInsert + c;
-		}
-
-		return ret ?? "";
-	}
-
-	static char ToWide(char c)
-		=> c switch
-		{
-			>= '\x21' and <= '\x7E' => (char)(c - '\x21' + '\xFF01'),
-			_ => c
 		};
 }
---
+++ TRViS/Utils/CharUtils.cs
@@ -0,0 +1,29 @@
+﻿namespace TRViS;
+
+public static partial class Utils
+{
+	public const string SPACE_CHAR = "\x2002";
+
+	static public string InsertCharBetweenCharAndMakeWide(string input, string toInsert)
+	{
+		string? ret = null;
+		foreach (char v in input)
+		{
+			char c = ToWide(v);
+
+			if (ret is null)
+				ret = c.ToString();
+			else
+				ret += toInsert + c;
+		}
+
+		return ret ?? "";
+	}
+
+	static public char ToWide(char c)
+		=> c switch
+		{
+			>= '\x21' and <= '\x7E' => (char)(c - '\x21' + '\xFF01'),
+			_ => c
+		};
+}
