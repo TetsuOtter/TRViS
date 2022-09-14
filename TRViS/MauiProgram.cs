82b7f9b1f5ce091ae0593146e9bd1323ae2ad613
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-09-14 16:35:19 +0900

    CommunityToolkitを読み込むようにした

--- TRViS/MauiProgram.cs
+++ TRViS/MauiProgram.cs
@@ -1,3 +1,5 @@
+using CommunityToolkit.Maui;
+
 namespace TRViS;
 
 public static class MauiProgram
@@ -12,7 +14,8 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 				fonts.AddFont("MaterialIcons-Regular.ttf", "MaterialIconsRegular");
-			});
+			})
+			.UseMauiCommunityToolkit();
 
 		return builder.Build();
 	}
