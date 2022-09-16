12ce78b8587ae207dde64653d1ad945e7322359d
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-09-16 16:02:50 +0900

    DB内のデータ構造に沿ってUIを変更

--- TRViS/SelectTrainPage.xaml.cs
+++ TRViS/SelectTrainPage.xaml.cs
@@ -4,10 +4,32 @@ namespace TRViS;
 
 public partial class SelectTrainPage : ContentPage
 {
+	AppViewModel viewModel { get; }
+
 	public SelectTrainPage(AppViewModel viewModel)
 	{
 		InitializeComponent();
 
+		this.viewModel = viewModel;
 		this.BindingContext = viewModel;
+	}
+
+	async void Button_Clicked(object sender, EventArgs e)
+	{
+		try
+		{
+			var result = await FilePicker.Default.PickAsync();
+
+			if (result is not null)
+			{
+				viewModel.Loader?.Dispose();
+
+				viewModel.Loader = new(result.FullPath);
+			}
+		}
+		catch (Exception ex)
+		{
+			await DisplayAlert("Cannot Open File", ex.ToString(), "OK");
+		}
 	}
 }
--- TRViS/ViewModels/AppViewModel.cs
+++ TRViS/ViewModels/AppViewModel.cs
@@ -2,14 +2,62 @@ using System.Collections.ObjectModel;
 
 using CommunityToolkit.Mvvm.ComponentModel;
 
+using TRViS.IO;
 using TRViS.IO.Models;
 
 namespace TRViS.ViewModels;
 
 public partial class AppViewModel : ObservableObject
 {
-	public ObservableCollection<TrainData> TrainDataList { get; } = new();
+	[ObservableProperty]
+	LoaderSQL? _Loader;
+
+	[ObservableProperty]
+	IReadOnlyList<TRViS.IO.Models.DB.WorkGroup>? _WorkGroupList;
+
+	[ObservableProperty]
+	IReadOnlyList<TRViS.IO.Models.DB.Work>? _WorkList;
+
+	[ObservableProperty]
+	IReadOnlyList<TRViS.IO.Models.DB.TrainData>? _DBTrainDataList;
+
+	[ObservableProperty]
+	TRViS.IO.Models.DB.WorkGroup? _SelectedWorkGroup;
+	[ObservableProperty]
+	TRViS.IO.Models.DB.Work? _SelectedWork;
+	[ObservableProperty]
+	TRViS.IO.Models.DB.TrainData? _SelectedDBTrainData;
 
 	[ObservableProperty]
 	TrainData? _SelectedTrainData;
+
+	partial void OnLoaderChanged(LoaderSQL? value)
+	{
+		WorkGroupList = null;
+		SelectedWorkGroup = null;
+
+		if (value is not null)
+			WorkGroupList = value.GetWorkGroupList();
+	}
+
+	partial void OnSelectedWorkGroupChanged(TRViS.IO.Models.DB.WorkGroup? value)
+	{
+		WorkList = null;
+		SelectedWork = null;
+
+		if (value is not null)
+			WorkList = Loader?.GetWorkList(value.Id);
+	}
+
+	partial void OnSelectedWorkChanged(IO.Models.DB.Work? value)
+	{
+		DBTrainDataList = null;
+		SelectedDBTrainData = null;
+
+		if (value is not null)
+			DBTrainDataList = Loader?.GetTrainDataList(value.Id);
+	}
+
+	partial void OnSelectedDBTrainDataChanged(IO.Models.DB.TrainData? value)
+		=> SelectedTrainData = value is null ? null : Loader?.GetTrainData(value.Id);
 }
