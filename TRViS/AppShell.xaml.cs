652f98c577f4f12c43ef787e37ceb20ce0cef98e
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-09-17 21:27:18 +0900

    EasterEggページを表示/機能させるようにした

--- TRViS/AppShell.xaml.cs
+++ TRViS/AppShell.xaml.cs
@@ -1,10 +1,17 @@
+using TRViS.ViewModels;
+
 namespace TRViS;
 
 public partial class AppShell : Shell
 {
-	public AppShell()
+	public AppShell(EasterEggPageViewModel easterEggPageViewModel)
 	{
 		InitializeComponent();
+
+		SetBinding(Shell.BackgroundColorProperty, new Binding() { Source = easterEggPageViewModel, Path = nameof(EasterEggPageViewModel.ShellBackgroundColor) });
+		SetBinding(Shell.TitleColorProperty, new Binding() { Source = easterEggPageViewModel, Path = nameof(EasterEggPageViewModel.ShellTitleTextColor) });
+
+		FlyoutIconImage.SetBinding(FontImageSource.ColorProperty, new Binding() { Source = easterEggPageViewModel, Path = nameof(EasterEggPageViewModel.ShellTitleTextColor) });
 	}
 }
 
