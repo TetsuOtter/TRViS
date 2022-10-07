ccad6b2d28d00293a979fca3b761776c79d26b90
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-10-07 14:03:00 +0900

    改行を入れてわかりやすくした

--- TRViS/Models/LicensesData.cs
+++ TRViS/Models/LicensesData.cs
@@ -1,3 +1,10 @@
 namespace TRViS.Models;
 
-public record LicenseData(string id, string resolvedVersion, string license, string author, string? projectUrl, string? copyrightText);
+public record LicenseData(
+	string id,
+	string resolvedVersion,
+	string license,
+	string author,
+	string? projectUrl,
+	string? copyrightText
+);
