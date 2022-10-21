62927558aabe52541dca74acaa01eb04a1f26e08
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-10-21 19:10:25 +0900

    Create IsOneOrMoreIntConverter.cs

---
+++ TRViS/ValueConverters/IsOneOrMoreIntConverter.cs
@@ -0,0 +1,14 @@
+using System.Globalization;
+
+namespace TRViS.ValueConverters;
+
+public class IsOneOrMoreIntConverter : IValueConverter
+{
+	static public IsOneOrMoreIntConverter Default { get; } = new();
+
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		=> value is int and >= 1;
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		=> throw new NotImplementedException();
+}
