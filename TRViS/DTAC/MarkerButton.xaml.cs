e492171e2fd5e9f366233d9bf14cb9d4431859d3
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-10-22 03:56:09 +0900

    モード変更機能を実装

--- TRViS/DTAC/MarkerButton.xaml.cs
+++ TRViS/DTAC/MarkerButton.xaml.cs
@@ -1,12 +1,39 @@
+using CommunityToolkit.Maui.Views;
+
 using DependencyPropertyGenerator;
+
+using TRViS.ViewModels;
 
 namespace TRViS.DTAC;
 
 [DependencyProperty<bool>("IsMarkModeToggled")]
 public partial class MarkerButton : Frame
 {
+	DTACMarkerViewModel VM { get; } = new();
+
 	public MarkerButton()
 	{
 		InitializeComponent();
+	}
+
+	async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+	{
+		if (Shell.Current.CurrentPage is not ViewHost page)
+			return;
+
+		if (IsMarkModeToggled)
+		{
+			IsMarkModeToggled = false;
+			return;
+		}
+
+		IsMarkModeToggled = true;
+
+		SelectMarkerPopup popup = new(VM)
+		{
+			Anchor = this,
+		};
+
+		await page.ShowPopupAsync(popup);
 	}
 }
