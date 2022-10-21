19e592056980c13b53e49fd4bdf82f0a0a89d419
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-10-21 19:37:59 +0900

    フルスクリーンで表示できるようにした

--- TRViS/Platforms/Android/MainActivity.cs
+++ TRViS/Platforms/Android/MainActivity.cs
@@ -1,11 +1,25 @@
 using Android.App;
 using Android.Content.PM;
-using Android.OS;
+using Android.Views;
 
 namespace TRViS;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
-}
+	public override void OnWindowFocusChanged(bool hasFocus)
+	{
+		base.OnWindowFocusChanged(hasFocus);
 
+		if (hasFocus && Window is not null)
+		{
+			// ref: https://developer.android.com/training/system-ui/immersive
+			SystemUiFlags flags
+				= SystemUiFlags.Immersive
+				| SystemUiFlags.HideNavigation
+				| SystemUiFlags.Fullscreen;
+
+			Window.DecorView.SystemUiVisibility = (StatusBarVisibility)(int)flags;
+		}
+	}
+}
