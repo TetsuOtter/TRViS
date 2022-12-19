d4f81abc84adeb517ad1d8efc69fe50691eef69d
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-12-19 19:57:50 +0900

    DB構造を更新

--- TRViS.IO.Tests/Resources/CreateTables.sql
+++ TRViS.IO.Tests/Resources/CreateTables.sql
@@ -1,6 +1,8 @@
 CREATE TABLE "work_group" (
 	"id"	INTEGER NOT NULL,
 	"name"	TEXT NOT NULL,
+
+	"db_version"	INTEGER,
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
 
@@ -9,6 +11,14 @@ CREATE TABLE "work" (
 	"work_group_id"	INTEGER NOT NULL,
 	"name"	TEXT NOT NULL,
 	"affect_date"	TEXT NOT NULL,
+
+	"affix_content_type"	INTEGER,
+	"affix_content"	BLOB,
+	"remarks" TEXT,
+	"has_e_train_timetable" INTEGER,
+	"e_train_timetable_content_type" INTEGER,
+	"e_train_timetable_content" BLOB,
+
 	FOREIGN KEY("work_group_id") REFERENCES "work_group"("id"),
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
@@ -28,7 +38,16 @@ CREATE TABLE "train_data" (
 	"before_departure"	TEXT,
 	"train_info"	TEXT,
 	"direction" INTEGER NOT NULL,
+
+	"after_arrive" TEXT,
+	"before_departure_on__station_track_col" TEXT,
+	"after_arrive_on__station_track_col" TEXT,
+	"day_count" INTEGER,
+	"is_ride_on_moving" INTEGER,
+	"color_id" INTEGER,
+
 	FOREIGN KEY("work_id") REFERENCES "work"("id"),
+	FOREIGN KEY("color_id") REFERENCES "color"("id"),
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
 
@@ -37,6 +56,13 @@ CREATE TABLE "station" (
 	"work_group_id"	INTEGER NOT NULL,
 	"name"	TEXT NOT NULL,
 	"location"	REAL NOT NULL,
+
+	"location_lon_deg"	REAL,
+	"location_lat_deg"	REAL,
+	"on_station_detect_radius_m"	REAL,
+	"full_name"	TEXT,
+	"record_type"	INTEGER,
+
 	PRIMARY KEY("id" AUTOINCREMENT),
 	FOREIGN KEY("work_group_id") REFERENCES "work_group"("id")
 );
@@ -71,9 +97,71 @@ CREATE TABLE "timetable_row" (
 	"remarks"	TEXT,
 	"arrive_str" TEXT,
 	"departure_str" TEXT,
+
+	"marker_color_id" INTEGER,
+	"marker_text" TEXT,
+	"work_type" INTEGER,
+
 	PRIMARY KEY("id" AUTOINCREMENT),
 	UNIQUE("train_id", "station_id"),
 	FOREIGN KEY("station_id") REFERENCES "station"("id"),
 	FOREIGN KEY("station_track_id") REFERENCES "station_track"("id"),
-	FOREIGN KEY("train_id") REFERENCES "train_data"("id")
+	FOREIGN KEY("train_id") REFERENCES "train_data"("id"),
+	FOREIGN KEY("marker_color_id") REFERENCES "color"("id")
+);
+
+CREATE TABLE "language" (
+	"id" INTEGER NOT NULL,
+	"language_code" TEXT NOT NULL,
+
+	PRIMARY KEY("id" AUTOINCREMENT),
+	UNIQUE("language_code")
+);
+
+CREATE TABLE "work_group_name_other_lang" (
+	"work_group_id" INTEGER NOT NULL,
+	"language_id" INTEGER NOT NULL,
+	"name" TEXT NOT NULL,
+
+	PRIMARY KEY("work_group_id", "language_id"),
+	FOREIGN KEY("work_group_id") REFERENCES "work_group"("id"),
+	FOREIGN KEY("language_id") REFERENCES "language"("id")
+);
+
+CREATE TABLE "work_name_other_lang" (
+	"work_id" INTEGER NOT NULL,
+	"language_id" INTEGER NOT NULL,
+	"name" TEXT NOT NULL,
+
+	PRIMARY KEY("work_id", "language_id"),
+	FOREIGN KEY("work_id") REFERENCES "work"("id"),
+	FOREIGN KEY("language_id") REFERENCES "language"("id")
+);
+
+CREATE TABLE "station_name_other_lang" (
+	"station_id" INTEGER NOT NULL,
+	"language_id" INTEGER NOT NULL,
+	"name" TEXT NOT NULL,
+
+	PRIMARY KEY("station_id", "language_id"),
+	FOREIGN KEY("station_id") REFERENCES "station"("id"),
+	FOREIGN KEY("language_id") REFERENCES "language"("id")
+);
+
+CREATE TABLE "station_track_name_other_lang" (
+	"station_track_id" INTEGER NOT NULL,
+	"language_id" INTEGER NOT NULL,
+	"name" TEXT NOT NULL,
+
+	PRIMARY KEY("station_track_id", "language_id"),
+	FOREIGN KEY("station_track_id") REFERENCES "station_track"("id"),
+	FOREIGN KEY("language_id") REFERENCES "language"("id")
+);
+
+CREATE TABLE "color" (
+	"id" INTEGER NOT NULL,
+	"name" TEXT NOT NULL,
+	"rgb" INTEGER NOT NULL,
+
+	PRIMARY KEY("id" AUTOINCREMENT)
 );
