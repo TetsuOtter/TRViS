c3f4579bf6f82099d9b94570562ec7eae6339392
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-12-22 16:44:20 +0900

    Crash Logを保存するようにした

--- TRViS/MauiProgram.cs
+++ TRViS/MauiProgram.cs
@@ -6,6 +6,29 @@ namespace TRViS;
 
 public static class MauiProgram
 {
+	static readonly string CrashLogFilePath;
+	public static readonly DirectoryInfo CrashLogFileDirectory;
+	static readonly string CrashLogFileName;
+
+	static MauiProgram()
+	{
+		CrashLogFileName = $"{DateTime.Now:yyyyMMdd_HHmmss}.crashlog.trvis.txt";
+
+		string baseDirPath;
+		if (DeviceInfo.Current.Platform == DevicePlatform.iOS)
+		{
+			baseDirPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+		}
+		else
+		{
+			baseDirPath = FileSystem.Current.AppDataDirectory;
+		}
+
+
+		CrashLogFileDirectory = new(Path.Combine(baseDirPath, "TRViS.InternalFiles", "crashlogs"));
+		CrashLogFilePath = Path.Combine(CrashLogFileDirectory.FullName, CrashLogFileName);
+	}
+
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
@@ -27,7 +50,22 @@ public static class MauiProgram
 			.AddSingleton(typeof(EasterEggPageViewModel))
 			.AddSingleton(typeof(AppViewModel));
 
+		AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
 		return builder.Build();
+	}
+
+	private static async void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+	{
+		if (e.ExceptionObject is not Exception ex)
+			return;
+
+		if (!CrashLogFileDirectory.Exists)
+		{
+			CrashLogFileDirectory.Create();
+		}
+
+		await File.AppendAllTextAsync(CrashLogFilePath, $"{DateTime.Now:[yyyy/MM/dd HH:mm:ss]} {ex.Message}\n{ex.StackTrace}\n---\n(InnerException: {ex.InnerException})\n\n");
 	}
 }
 
