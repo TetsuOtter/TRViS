c490d47d8fd56ec68da1dddc173ba52296db2729
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2023-01-21 23:33:02 +0900

    BeforeDeparture部分を`BeforeDeparture_AfterArrive`に移動

--- TRViS/DTAC/TrainInfo_BeforeDeparture.cs
+++ TRViS/DTAC/TrainInfo_BeforeDeparture.cs
@@ -13,13 +13,6 @@ public partial class TrainInfo_BeforeDeparture : Grid
 		new RowDefinition(DEFAULT_ROW_HEIGHT)
 	};
 
-	static readonly ColumnDefinitionCollection DefaultColumnDefinitions = new()
-	{
-		new ColumnDefinition(VerticalStylePage.RUN_TIME_COLUMN_WIDTH),
-		new ColumnDefinition(140 * 3),
-		new ColumnDefinition(new(1, GridUnitType.Star)),
-	};
-
 	#region TrainInfo Area
 	readonly HtmlAutoDetectLabel TrainInfoArea = DTACElementStyles.LargeLabelStyle<HtmlAutoDetectLabel>();
 
@@ -30,48 +23,34 @@ public partial class TrainInfo_BeforeDeparture : Grid
 	}
 	#endregion
 
-	#region TrainInfo Area
-	readonly HtmlAutoDetectLabel BeforeDepartureArea = DTACElementStyles.LabelStyle<HtmlAutoDetectLabel>();
-	readonly HtmlAutoDetectLabel BeforeDepartureArea_OnStationTrackColumn = DTACElementStyles.LabelStyle<HtmlAutoDetectLabel>();
+	#region Before Departure Area
+	readonly BeforeDeparture_AfterArrive BeforeDeparture;
 
 	public string BeforeDepartureText
 	{
-		get => BeforeDepartureArea.Text;
-		set => BeforeDepartureArea.Text = value;
+		get => BeforeDeparture.Text;
+		set => BeforeDeparture.Text = value;
 	}
 
 	public string BeforeDepartureText_OnStationTrackColumn
 	{
-		get => BeforeDepartureArea_OnStationTrackColumn.Text;
-		set => BeforeDepartureArea_OnStationTrackColumn.Text = value;
+		get => BeforeDeparture.Text_OnStationTrackColumn;
+		set => BeforeDeparture.Text_OnStationTrackColumn = value;
 	}
 	#endregion
 
 	public TrainInfo_BeforeDeparture()
 	{
 		RowDefinitions = DefaultRowDefinitions;
-		ColumnDefinitions = DefaultColumnDefinitions;
+		ColumnDefinitions = DTACElementStyles.TimetableColumnWidthCollection;
 
 		Line trainInfoAreaSeparator = DTACElementStyles.HorizontalSeparatorLineStyle();
 		TrainInfoArea.HorizontalOptions = LayoutOptions.Start;
 
 		// BeforeDepartureArea
-		Line beforeDepartureAreaSeparator = DTACElementStyles.HorizontalSeparatorLineStyle();
-		BoxView beforeDepartureHeaderBoxView = new()
-		{
-			BackgroundColor = DTACElementStyles.HeaderBackgroundColor,
-			Color = DTACElementStyles.HeaderBackgroundColor,
-			Margin = new(0),
-		};
-		Label beforeDepartureHeaderLabel = DTACElementStyles.HeaderLabelStyle<Label>();
-		beforeDepartureHeaderLabel.HorizontalOptions = LayoutOptions.Center;
-		beforeDepartureHeaderLabel.Text = "発前";
+		BeforeDeparture = new(this, "発前");
 
-		BeforeDepartureArea.HorizontalOptions = LayoutOptions.Start;
-		BeforeDepartureArea_OnStationTrackColumn.HorizontalOptions = LayoutOptions.Start;
-
-		Grid.SetColumnSpan(trainInfoAreaSeparator, 3);
-		Grid.SetColumnSpan(TrainInfoArea, 3);
+		Grid.SetColumnSpan(TrainInfoArea, 8);
 		this.Add(
 			trainInfoAreaSeparator,
 			row: 0
@@ -81,29 +60,7 @@ public partial class TrainInfo_BeforeDeparture : Grid
 			row: 0
 		);
 
-		Grid.SetColumnSpan(beforeDepartureAreaSeparator, 3);
-		Grid.SetColumnSpan(BeforeDepartureArea, 2);
-		this.Add(
-			beforeDepartureAreaSeparator,
-			row: 1
-		);
-		this.Add(
-			beforeDepartureHeaderBoxView,
-			row: 1
-		);
-		this.Add(
-			beforeDepartureHeaderLabel,
-			row: 1
-		);
-		this.Add(
-			BeforeDepartureArea,
-			column: 1,
-			row: 1
-		);
-		this.Add(
-			BeforeDepartureArea_OnStationTrackColumn,
-			column: 2,
-			row: 1
-		);
+		BeforeDeparture.AddToParent();
+		BeforeDeparture.SetRow(1);
 	}
 }
