e57a8f03b07a40cdc7ab07bbe6cf9284754b888a
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2023-03-29 01:23:52 +0900

    Location ServiceのIntervalを5秒に設定
    最終的には良い感じに変更できるようにしたい

--- TRViS/DTAC/VerticalTimetableView.Init.cs
+++ TRViS/DTAC/VerticalTimetableView.Init.cs
@@ -44,6 +44,8 @@ public partial class VerticalTimetableView
 		{
 			MainThread.BeginInvokeOnMainThread(() => Shell.Current.DisplayAlert("Location Service Error", e.ToString(), "OK"));
 		};
+
+		LocationService.Interval = new TimeSpan(0, 0, 5);
 	}
 
 	async void SetRowViews(TrainData? trainData, TimetableRow[]? newValue)
