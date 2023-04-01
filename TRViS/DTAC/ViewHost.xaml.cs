e716682f07160d609ba573615732f4bd310d5b03
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2023-04-01 13:56:27 +0900

    Shell Title部分を独自実装に変更

--- TRViS/DTAC/ViewHost.xaml.cs
+++ TRViS/DTAC/ViewHost.xaml.cs
@@ -5,11 +5,23 @@ namespace TRViS.DTAC;
 
 public partial class ViewHost : ContentPage
 {
+	static public readonly GridLength TitleViewHeight = new(45, GridUnitType.Absolute);
+
 	DTACViewHostViewModel ViewModel { get; }
 
-	public ViewHost(AppViewModel vm)
+	public ViewHost(AppViewModel vm, EasterEggPageViewModel eevm)
 	{
+		Shell.SetNavBarIsVisible(this, false);
+
 		InitializeComponent();
+
+		TitleLabel.Text = vm.SelectedWork?.Name;
+		TitleLabel.TextColor = MenuButton.TextColor = eevm.ShellTitleTextColor;
+
+		TitleBGBoxView.BackgroundColor = eevm.ShellBackgroundColor;
+
+		vm.PropertyChanged += Vm_PropertyChanged;
+		eevm.PropertyChanged += Eevm_PropertyChanged;
 
 		ViewModel = new(vm);
 		BindingContext = ViewModel;
@@ -29,6 +41,34 @@ public partial class ViewHost : ContentPage
 		});
 
 		UpdateContent();
+	}
+
+	private void MenuButton_Clicked(object? sender, EventArgs e)
+	{
+		Shell.Current.FlyoutIsPresented = !Shell.Current.FlyoutIsPresented;
+	}
+
+	private void Eevm_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+	{
+		if (sender is not EasterEggPageViewModel vm)
+			return;
+
+		switch (e.PropertyName)
+		{
+			case nameof(EasterEggPageViewModel.ShellBackgroundColor):
+				TitleBGBoxView.Color = vm.ShellBackgroundColor;
+				break;
+
+			case nameof(EasterEggPageViewModel.ShellTitleTextColor):
+				TitleLabel.TextColor = MenuButton.TextColor = vm.ShellTitleTextColor;
+				break;
+		}
+	}
+
+	private void Vm_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+	{
+		if (e.PropertyName == nameof(AppViewModel.SelectedWork))
+			TitleLabel.Text = (sender as AppViewModel)?.SelectedWork?.Name;
 	}
 
 	private void ViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
