1b8fcb414f445a804a76c3e6d0af01c4787e4661
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2023-06-07 01:48:32 +0900

    Create AppThemeColorBindingExtension.cs

---
+++ TRViS/Utils/AppThemeColorBindingExtension.cs
@@ -0,0 +1,26 @@
+namespace TRViS;
+
+public class AppThemeGenericsBindingExtension<T> : AppThemeBindingExtension where T : class
+{
+	public new T? Dark => base.Dark as T;
+	public new T? Light => base.Light as T;
+	public new T? Default => base.Default as T;
+	public new T? Value => base.Value as T;
+
+	public AppThemeGenericsBindingExtension(T Default, T Dark)
+	{
+		base.Default = Default;
+		base.Dark = Dark;
+	}
+
+	public virtual void Apply(VisualElement? elem, BindableProperty prop)
+		=> elem?.SetAppTheme(prop, this.Light, this.Dark);
+}
+
+public class AppThemeColorBindingExtension : AppThemeGenericsBindingExtension<Color>
+{
+	public AppThemeColorBindingExtension(Color Default, Color Dark) : base(Default, Dark) { }
+
+	public override void Apply(VisualElement? elem, BindableProperty prop)
+		=> elem?.SetAppThemeColor(prop, this.Light, this.Dark);
+}
