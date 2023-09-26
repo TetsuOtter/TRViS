aebc9d706f2eb76080bb65172b26c4c62167c7cb
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2023-09-27 01:21:33 +0900

    AppThemeが設定されていない場合に正常に色が設定されないバグを修正
    AppThemeが設定されていないと、HTMLで設定したcolorが反映されない状態だった (設定されたTextColorが優先されていた)

--- TRViS/Controls/HtmlAutoDetectLabel.cs
+++ TRViS/Controls/HtmlAutoDetectLabel.cs
@@ -5,6 +5,7 @@ namespace TRViS.Controls;
 public class HtmlAutoDetectLabel : Label
 {
 	public AppThemeColorBindingExtension? CurrentAppThemeColorBindingExtension { get; set; }
+	public Color? LastTextColor {get; private set; }
 
 	protected override void OnPropertyChanged([CallerMemberName] string? propertyName = null)
 	{
@@ -17,20 +18,32 @@ public class HtmlAutoDetectLabel : Label
 			else
 			{
 				string text = Text.Trim();
+				bool isColoredString = text.Contains("color:");
 
 				try
 				{
 					TextType _textType = (text.StartsWith('<') && text.EndsWith('>')) ? TextType.Html : TextType.Text;
 					if (CurrentAppThemeColorBindingExtension is not null)
 					{
-						if (_textType == TextType.Html
-							&& text.Contains("color:"))
+						if (_textType == TextType.Html && isColoredString)
 						{
 							this.SetAppThemeColor(TextColorProperty, null, null);
 						}
 						else
 						{
 							CurrentAppThemeColorBindingExtension.Apply(this, TextColorProperty);
+						}
+					}
+					else
+					{
+						if (_textType == TextType.Html && isColoredString)
+						{
+							LastTextColor = TextColor;
+							TextColor = null;
+						}
+						else if (TextColor is null && LastTextColor is not null)
+						{
+							TextColor = LastTextColor;
 						}
 					}
 					TextType = _textType;
