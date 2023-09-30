83589641fae14f794b3143a0f174b4876bae0fd2
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2023-09-30 15:26:04 +0900

    とりあえずTitleBar部分の色をJSONに移行

--- TRViS/ViewModels/EasterEggPageViewModel.cs
+++ TRViS/ViewModels/EasterEggPageViewModel.cs
@@ -1,9 +1,12 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using TRViS.MyAppCustomizables;
 
 namespace TRViS.ViewModels;
 
 public partial class EasterEggPageViewModel : ObservableObject
 {
+	private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
 	[ObservableProperty]
 	Color _ShellBackgroundColor = Colors.Black;
 
@@ -19,26 +22,68 @@ public partial class EasterEggPageViewModel : ObservableObject
 
 	public EasterEggPageViewModel()
 	{
-		if (Preferences.Default.ContainsKey(nameof(ShellBackgroundColor)))
+		logger.Trace("EasterEggPageViewModel Creating (with Task.Run)");
+
+		Task.Run(LoadFromFileAsync);
+	}
+
+	public async Task LoadFromFileAsync()
+	{
+		logger.Info("Loading SettingFileStructure from default setting file");
+		(SettingFileStructure settingFile, string? msg) = await SettingFileStructure.LoadFromJsonFileOrCreateAsync();
+
+		await InitAsync(settingFile, msg == SettingFileStructure.settingFileCreatedMsg);
+	}
+
+	public async Task LoadFromFileAsync(string path)
+	{
+		(SettingFileStructure settingFile, string? msg) tmp;
+
+		logger.Info("Loading SettingFileStructure from setting file (path: {0})", path);
+		using (FileStream stream = File.OpenRead(path))
+		{
+			tmp = await SettingFileStructure.LoadFromJsonAsync(stream);
+		}
+
+		await InitAsync(tmp.settingFile, tmp.msg == SettingFileStructure.settingFileCreatedMsg);
+	}
+
+	async Task InitAsync(SettingFileStructure settingFile, bool isNewlyCreated)
+	{
+		logger.Debug("InitAsync (setting: {0}, isNewlyCreated: {1})", settingFile, isNewlyCreated);
+		if (isNewlyCreated && Preferences.Default.ContainsKey(nameof(ShellBackgroundColor)))
 		{
 			int value = Preferences.Default.Get<int>(nameof(ShellBackgroundColor), 0);
 
-			_ShellBackgroundColor = Color.FromInt(value);
-
-			_Color_Red = (int)(ShellBackgroundColor.Red * 255);
-			_Color_Green = (int)(ShellBackgroundColor.Green * 255);
-			_Color_Blue = (int)(ShellBackgroundColor.Blue * 255);
-
-			SetTitleTextColor();
+			logger.Info("Setting title color stored in AppPreference (value: {0:X6})", value);
+			settingFile.TitleColor = new(Color.FromInt(value));
+			await settingFile.SaveToJsonFileAsync();
 		}
+
+		ShellBackgroundColor = settingFile.TitleColor.ToColor();
+		Color_Red = settingFile.TitleColor.Red;
+		Color_Green = settingFile.TitleColor.Green;
+		Color_Blue = settingFile.TitleColor.Blue;
+
+		SetTitleTextColor();
+		logger.Trace("InitAsync Completed");
+	}
+
+	public async Task SaveAsync()
+	{
+		logger.Info("Saving setting to file...");
+		SettingFileStructure settingFile = new()
+		{
+			TitleColor = new(ShellBackgroundColor),
+		};
+
+		await settingFile.SaveToJsonFileAsync();
 	}
 
 	partial void OnShellBackgroundColorChanged(Color value)
 	{
 		if (value is not null)
 		{
-			Preferences.Default.Set<int>(nameof(ShellBackgroundColor), value.ToInt());
-
 			SetTitleTextColor();
 		}
 	}
