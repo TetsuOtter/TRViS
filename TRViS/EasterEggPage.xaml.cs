c2618e0c443ee8f81aafa224467f14222f0c77c1
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2023-09-30 18:08:07 +0900

    設定の保存/読み込み機能を実装

--- TRViS/EasterEggPage.xaml.cs
+++ TRViS/EasterEggPage.xaml.cs
@@ -1,3 +1,4 @@
+using TRViS.MyAppCustomizables;
 using TRViS.ViewModels;
 
 namespace TRViS;
@@ -5,6 +6,7 @@ namespace TRViS;
 public partial class EasterEggPage : ContentPage
 {
 	private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+	EasterEggPageViewModel ViewModel { get; }
 
 	public EasterEggPage(EasterEggPageViewModel vm)
 	{
@@ -13,9 +15,38 @@ public partial class EasterEggPage : ContentPage
 		InitializeComponent();
 
 		BindingContext = vm;
+		ViewModel = vm;
 
 		LogFilePathLabel.Text = DirectoryPathProvider.NormalLogFileDirectory.FullName;
 
 		logger.Trace("EasterEggPage Created");
+	}
+
+	private void OnLoadFromPickerClicked(object sender, EventArgs e)
+	{
+		logger.Warn("Not Implemented");
+	}
+	private void OnSaveToPickerClicked(object sender, EventArgs e)
+	{
+		logger.Trace("Not Implemented");
+	}
+
+	private async void OnReloadSavedClicked(object sender, EventArgs e)
+	{
+		logger.Trace("Executing...");
+
+		await ViewModel.LoadFromFileAsync();
+
+		logger.Info("Reload Complete");
+	}
+
+	private async void OnSaveClicked(object sender, EventArgs e)
+	{
+		logger.Trace("Executing...");
+
+		await ViewModel.SaveAsync();
+
+		logger.Info("Saved");
+		await DisplayAlert("Success!", "Successfully saved", "OK");
 	}
 }
