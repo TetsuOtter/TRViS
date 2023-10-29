5e13f9c0237b5fa0b0e9093227cde8500b21a30f
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2023-10-29 22:48:43 +0900

    ShowMdPageからSimpleMarkdownViewに移行
    他の場所でもMDを扱いたくなったため

--- TRViS/RootPages/ShowMarkdownPage.cs
+++ TRViS/RootPages/ShowMarkdownPage.cs
@@ -8,7 +8,7 @@ namespace TRViS.RootPages;
 public partial class ShowMarkdownPage : ContentPage
 {
 	private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
-	readonly SimpleMarkdownLabel markdownLabel;
+	readonly SimpleMarkdownView markdownView;
 
 	public ShowMarkdownPage()
 	{
@@ -17,8 +17,8 @@ public partial class ShowMarkdownPage : ContentPage
 		{
 			Padding = new Thickness(10),
 		};
-		markdownLabel = new();
-		scrollView.Content = markdownLabel;
+		markdownView = new();
+		scrollView.Content = markdownView;
 		Content = scrollView;
 
 		logger.Trace("Created.");
@@ -26,33 +26,6 @@ public partial class ShowMarkdownPage : ContentPage
 
 	partial void OnFileNameChanged(string? oldValue, string? newValue)
 	{
-		logger.Debug("OnFileNameChanged: '{0}' -> '{1}'", oldValue, newValue);
-		markdownLabel.MarkdownFileContent = string.Empty;
-		if (string.IsNullOrEmpty(newValue))
-		{
-			logger.Debug("newValue is null or empty.");
-			return;
-		}
-
-		Task.Run(async () => {
-			bool isExist = await FileSystem.AppPackageFileExistsAsync(newValue);
-			if (!isExist)
-			{
-				logger.Warn("File not found: '{0}'", newValue);
-				return;
-			}
-
-			try
-			{
-				using Stream stream = await FileSystem.OpenAppPackageFileAsync(newValue);
-				using StreamReader reader = new(stream);
-				string fileContent = await reader.ReadToEndAsync();
-				markdownLabel.MarkdownFileContent = fileContent;
-			}
-			catch (Exception ex)
-			{
-				logger.Error(ex, "Open file failed: '{0}'", newValue);
-			}
-		});
+		markdownView.FileName = newValue;
 	}
 }
