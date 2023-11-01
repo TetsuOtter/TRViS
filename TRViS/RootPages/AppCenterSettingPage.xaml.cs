64434baee61bed1a7f9484eef90a090919119620
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2023-11-02 00:43:21 +0900

    値の保存/反映/ページ移動等の処理を実装

--- TRViS/RootPages/AppCenterSettingPage.xaml.cs
+++ TRViS/RootPages/AppCenterSettingPage.xaml.cs
@@ -5,11 +5,47 @@ namespace TRViS.RootPages;
 public partial class AppCenterSettingPage : ContentPage
 {
 	public static readonly string NameOfThisClass = nameof(AppCenterSettingPage);
+	private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
 
 	AppCenterSettingViewModel AppCenterSettingViewModel { get; }
 	public AppCenterSettingPage()
 	{
+		logger.Trace("Creating");
 		AppCenterSettingViewModel = new(InstanceManager.AppCenterSettingViewModel);
+		BindingContext = AppCenterSettingViewModel;
 		InitializeComponent();
+		logger.Trace("Created");
+	}
+
+	private async void OnResetButtonClicked(object? sender, EventArgs e)
+	{
+		logger.Trace("Executing...");
+		AppCenterSettingViewModel.CopyFrom(InstanceManager.AppCenterSettingViewModel);
+		AppCenterSettingViewModel.IsEnabled = false;
+		await AppCenterSettingViewModel.SaveAndApplySettings(true);
+		logger.Trace("Executed");
+	}
+
+	private async void OnSaveButtonClicked(object? sender, EventArgs e)
+	{
+		logger.Trace("Executing...");
+
+		bool isInitialSetting = !AppCenterSettingViewModel.IsEnabled;
+		logger.Debug("IsInitialSetting: {0}", isInitialSetting);
+		AppCenterSettingViewModel.IsEnabled = true;
+
+		InstanceManager.AppCenterSettingViewModel.CopyFrom(AppCenterSettingViewModel);
+		await InstanceManager.AppCenterSettingViewModel.SaveAndApplySettings(true);
+		logger.Trace("SaveAndApply Executed");
+
+		string installId = await AppCenterSettingViewModel.GetInstallId();
+		await DisplayAlert("Success!", "Successfully saved\nYour InstallId: " + installId, "OK");
+
+		// 初回はこのページが自動で表示されている状態のため、自動でページを移動するようにする
+		// 次回以降はユーザが自分で移動してきたはずであるため、自動で移動しないようにする
+		if (isInitialSetting)
+		{
+			await Shell.Current.GoToAsync("//" + nameof(SelectTrainPage));
+		}
 	}
 }
