838358b6f52507778da5dfadcffad76dc54c9197
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2023-11-06 23:43:12 +0900

    スタイルを修正

--- TRViS/DTAC/HakoParts/SimpleRow.cs
+++ TRViS/DTAC/HakoParts/SimpleRow.cs
@@ -9,16 +9,94 @@ public partial class SimpleRow
 {
 	private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
 
+	static readonly Thickness StaNameTrainNumButtonMargin = new(0, 4);
+	static readonly Thickness TrainNumButtonPadding = new(32, 4);
+	static readonly double TimeLabelFontSize_HHMM = DTACElementStyles.LargeTextSize * 0.8;
+	static readonly double TimeLabelFontSize_SS = TimeLabelFontSize_HHMM * 0.7;
+	static readonly double TrainNumberLabelTextSize = DTACElementStyles.LargeTextSize * 0.8;
+
 	public event ValueChangedEventHandler<bool>? IsSelectedChanged;
 
-	readonly Label FromStationNameLabel = DTACElementStyles.LargeLabelStyle<Label>();
-	readonly Label FromTimeLabel = DTACElementStyles.LabelStyle<Label>();
-	readonly Label ToStationNameLabel = DTACElementStyles.LargeLabelStyle<Label>();
-	readonly Label ToTimeLabel = DTACElementStyles.LabelStyle<Label>();
+	readonly Label FromStationNameLabel;
+	readonly Label FromTimeLabel;
+	readonly Label ToStationNameLabel;
+	readonly Label ToTimeLabel;
+	static Label GenStationNameLabel(int rowIndex, int colIndex)
+	{
+		Label v = DTACElementStyles.LargeLabelStyle<Label>();
+		v.VerticalOptions = LayoutOptions.End;
+		v.HorizontalOptions = LayoutOptions.Center;
+		v.Margin = StaNameTrainNumButtonMargin;
 
-	readonly Frame SelectTrainButtonFrame = new();
+		Grid.SetRow(v, rowIndex);
+		Grid.SetColumn(v, colIndex);
+
+		return v;
+	}
+	static Label GenTimeLabel(int rowIndex, int colIndex)
+	{
+		Label v = DTACElementStyles.LabelStyle<Label>();
+		v.VerticalOptions = LayoutOptions.Center;
+		v.HorizontalOptions = LayoutOptions.Center;
+
+		Grid.SetRow(v, rowIndex);
+		Grid.SetColumn(v, colIndex);
+
+		return v;
+	}
+
+	readonly Frame SelectTrainButtonFrame;
+	static readonly Color SelectTrainButtonFrameBorderColor = new(0.6f);
+	static readonly Shadow SelectTrainButtonFrameShadow = new()
+	{
+		Brush = Colors.Black,
+		Offset = new(0, 0),
+		Radius = 4,
+		Opacity = 0.4f,
+	};
+	static Frame GenSelectTrainButtonFrame(Label TrainNumberLabel)
+	{
+		Frame v = new()
+		{
+			Margin = StaNameTrainNumButtonMargin,
+			Padding = TrainNumButtonPadding,
+			VerticalOptions = LayoutOptions.End,
+			HorizontalOptions = LayoutOptions.Center,
+			MinimumWidthRequest = 120,
+			HasShadow = true,
+			Shadow = SelectTrainButtonFrameShadow,
+			BorderColor = SelectTrainButtonFrameBorderColor,
+		};
+		DTACElementStyles.DefaultBGColor.Apply(v, Frame.BackgroundColorProperty);
+		v.Content = TrainNumberLabel;
+
+		return v;
+	}
+
 	readonly ToggleButton SelectTrainButton = new();
-	readonly Label TrainNumberLabel = DTACElementStyles.LargeLabelStyle<Label>();
+	static ToggleButton GenSelectTrainButton(Frame SelectTrainButtonFrame, EventHandler<ValueChangedEventArgs<bool>> IsSelectedChanged, int rowIndex)
+	{
+		ToggleButton v = new()
+		{
+			Content = SelectTrainButtonFrame
+		};
+
+		Grid.SetColumn(v, 1);
+		Grid.SetRow(v, rowIndex);
+
+		v.IsCheckedChanged += IsSelectedChanged;
+
+		return v;
+	}
+	readonly Label TrainNumberLabel;
+	static Label GenTrainNumberLabel()
+	{
+		Label v = DTACElementStyles.LargeLabelStyle<Label>();
+
+		v.FontSize = TrainNumberLabelTextSize;
+
+		return v;
+	}
 
 	readonly Line RouteLine = new()
 	{
@@ -26,6 +104,22 @@ public partial class SimpleRow
 		VerticalOptions = LayoutOptions.Center,
 		HorizontalOptions = LayoutOptions.Fill,
 	};
+	static Line GenRouteLine(int rowIndex)
+	{
+		Line v = new()
+		{
+			StrokeThickness = 2,
+			VerticalOptions = LayoutOptions.Center,
+			HorizontalOptions = LayoutOptions.Fill,
+		};
+
+		DTACElementStyles.ForegroundBlackWhite.Apply(v, Line.BackgroundColorProperty);
+
+		Grid.SetColumn(v, 1);
+		Grid.SetRow(v, rowIndex);
+
+		return v;
+	}
 
 	readonly Grid ParentGrid;
 
@@ -35,36 +129,27 @@ public partial class SimpleRow
 
 		ParentGrid = parentGrid;
 
-		SelectTrainButton.IsCheckedChanged += (sender, e) =>
-		{
-			IsSelectedChanged?.Invoke(this, e.OldValue, e.NewValue);
-		};
-		SelectTrainButton.Content = SelectTrainButtonFrame;
-		DTACElementStyles.DefaultBGColor.Apply(SelectTrainButtonFrame, Frame.BackgroundColorProperty);
-		SelectTrainButtonFrame.Content = TrainNumberLabel;
-
-		DTACElementStyles.ForegroundBlackWhite.Apply(RouteLine, Line.BackgroundColorProperty);
-
 		int rowIndex_StaName_SelectBtn = dataIndex * 2;
 		int rowIndex_time = rowIndex_StaName_SelectBtn + 1;
-		Grid.SetRow(FromStationNameLabel, rowIndex_StaName_SelectBtn);
-		Grid.SetRow(SelectTrainButton, rowIndex_StaName_SelectBtn);
-		Grid.SetRow(ToStationNameLabel, rowIndex_StaName_SelectBtn);
 
-		Grid.SetRow(FromTimeLabel, rowIndex_time);
-		Grid.SetRow(RouteLine, rowIndex_time);
-		Grid.SetRow(ToTimeLabel, rowIndex_time);
+		TrainNumberLabel = GenTrainNumberLabel();
+		SelectTrainButtonFrame = GenSelectTrainButtonFrame(TrainNumberLabel);
+		SelectTrainButton = GenSelectTrainButton(SelectTrainButtonFrame, (sender, e) =>
+		{
+			IsSelectedChanged?.Invoke(this, e.OldValue, e.NewValue);
+			SetTrainNumberButtonState();
+		}, rowIndex_StaName_SelectBtn);
 
-		Grid.SetColumn(FromStationNameLabel, 0);
-		Grid.SetColumn(SelectTrainButton, 1);
-		Grid.SetColumn(ToStationNameLabel, 2);
+		RouteLine = GenRouteLine(rowIndex_time);
 
-		Grid.SetColumn(FromTimeLabel, 0);
-		Grid.SetColumn(RouteLine, 1);
-		Grid.SetColumn(ToTimeLabel, 2);
+		FromStationNameLabel = GenStationNameLabel(rowIndex_StaName_SelectBtn, 0);
+		ToStationNameLabel = GenStationNameLabel(rowIndex_StaName_SelectBtn, 2);
 
-		parentGrid.Add(FromStationNameLabel);
+		FromTimeLabel = GenTimeLabel(rowIndex_time, 0);
+		ToTimeLabel = GenTimeLabel(rowIndex_time, 2);
+
 		parentGrid.Add(SelectTrainButton);
+		parentGrid.Add(FromStationNameLabel);
 		parentGrid.Add(ToStationNameLabel);
 
 		parentGrid.Add(FromTimeLabel);
@@ -72,6 +157,30 @@ public partial class SimpleRow
 		parentGrid.Add(ToTimeLabel);
 
 		logger.Debug("Created");
+	}
+
+	void SetTrainNumberButtonState()
+	{
+		if (SelectTrainButton.IsEnabled && SelectTrainButton.IsChecked)
+		{
+			DTACElementStyles.DefaultGreen.Apply(SelectTrainButtonFrame, Frame.BorderColorProperty);
+			SelectTrainButtonFrame.HasShadow = false;
+		}
+		else
+		{
+			SelectTrainButtonFrame.BorderColor = SelectTrainButtonFrameBorderColor;
+			SelectTrainButtonFrame.HasShadow = true;
+		}
+	}
+
+	public bool IsEnabled
+	{
+		get => SelectTrainButton.IsEnabled;
+		set
+		{
+			SelectTrainButton.IsEnabled = value;
+			SetTrainNumberButtonState();
+		}
 	}
 
 	public string FromStationName
@@ -87,7 +196,7 @@ public partial class SimpleRow
 	public string TrainNumber
 	{
 		get => TrainNumberLabel.Text;
-		set => TrainNumberLabel.Text = value;
+		set => TrainNumberLabel.Text = Utils.ToWide(value);
 	}
 
 	private TimeData? fromTime;
@@ -125,7 +234,7 @@ public partial class SimpleRow
 	{
 		FormattedString fs = new();
 
-		double baseTextSize = DTACElementStyles.LargeTextSize * 0.8;
+		double baseTextSize = TimeLabelFontSize_HHMM;
 		fs.Spans.Add(new(){
 			Text = $"{value.Hour:00}:{value.Minute:00}",
 			FontAttributes = FontAttributes.Bold,
@@ -133,7 +242,7 @@ public partial class SimpleRow
 		});
 		fs.Spans.Add(new(){
 			Text = value.Second?.ToString("00"),
-			FontSize = baseTextSize * 0.75,
+			FontSize = TimeLabelFontSize_SS,
 		});
 
 		return fs;
