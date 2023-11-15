849edc514735500dff837f79d2e5c36b4b061e05
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2023-11-15 21:21:19 +0900

    Leave判定用の関数も追加

--- TRViS.LocationService/Utils/IsNearBy.cs
+++ TRViS.LocationService/Utils/IsNearBy.cs
@@ -5,5 +5,15 @@ internal static partial class Utils
 	public static bool IsNearBy(StaLocationInfo target, ILocationLonLat_deg currentLocation)
 		=> IsNearBy(target, CalculateDistance_m(target, currentLocation));
 	public static bool IsNearBy(StaLocationInfo target, double currentDistance_m)
-		=> currentDistance_m <= target.NearbyRadius_m;
+		=> IsNearBy(target.NearbyRadius_m, currentDistance_m);
+
+	public static bool IsNearBy(double NearbyRadius_m, double currentDistance_m)
+		=> currentDistance_m <= NearbyRadius_m;
+	
+	public static bool IsLeaved(StaLocationInfo target, ILocationLonLat_deg currentLocation)
+		=> IsLeaved(target, CalculateDistance_m(target, currentLocation));
+	public static bool IsLeaved(StaLocationInfo target, double currentDistance_m)
+		=> IsLeaved(target.NearbyRadius_m, currentDistance_m);
+	public static bool IsLeaved(double NearbyRadius_m, double currentDistance_m)
+		=> !IsNearBy(NearbyRadius_m * 1.1, currentDistance_m);
 }
