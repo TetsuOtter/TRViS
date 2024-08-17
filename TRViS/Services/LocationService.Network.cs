3050b7d2db3afbd97016fffcfd9248deee8d95e4
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2024-08-17 12:27:34 +0900

    NetworkSyncService リトライを実装
    とりあえず10回。将来的に「再施行しますか?」は付けても良いかも

--- TRViS/Services/LocationService.Network.cs
+++ TRViS/Services/LocationService.Network.cs
@@ -4,8 +4,10 @@ namespace TRViS.Services;
 
 public partial class LocationService
 {
+	const int EXCEPTION_MAX = 10;
 	async Task NetworkSyncServiceTask(NetworkSyncService networkService, CancellationToken token)
 	{
+		int exceptionCounter = 0;
 		while (!token.IsCancellationRequested)
 		{
 			logger.Trace("NetworkSyncServiceTask Loop");
@@ -23,11 +25,28 @@ public partial class LocationService
 					// Network経由の場合は負荷が少ないため、頻度が高くても問題ないはず
 					Task.Delay(Interval / 5, token)
 				);
+				exceptionCounter = 0;
 			}
-			catch (TaskCanceledException)
+			catch (TaskCanceledException exTask)
 			{
-				logger.Debug("Task is canceled -> break");
-				break;
+				if (token.IsCancellationRequested)
+				{
+					logger.Debug("Cancellation is requested -> break");
+					break;
+				}
+
+				logger.Debug("Task is canceled -> treat as exception");
+				if (exceptionCounter++ <= EXCEPTION_MAX)
+				{
+					logger.Warn("Exception Counter: {0}", exceptionCounter);
+					await Task.Delay(Interval, token);
+					continue;
+				}
+
+				logger.Warn("Switching to GpsPositioningTask");
+				SetLonLatLocationService();
+				ExceptionThrown?.Invoke(this, exTask);
+				return;
 			}
 			catch (Exception ex)
 			{
@@ -35,13 +54,15 @@ public partial class LocationService
 				IsEnabled = false;
 				serviceCancellation?.Cancel();
 
-				object? o = _CurrentService;
-				if (ReferenceEquals(o, networkService))
-						SetLonLatLocationService();
-				if (ExceptionThrown is null)
-					throw;
-				else
-					ExceptionThrown.Invoke(this, ex);
+				if (exceptionCounter++ <= EXCEPTION_MAX)
+				{
+					logger.Warn("Exception Counter: {0}", exceptionCounter);
+					await Task.Delay(Interval, token);
+					continue;
+				}
+				logger.Warn("Switching to GpsPositioningTask");
+				SetLonLatLocationService();
+				ExceptionThrown?.Invoke(this, ex);
 				return;
 			}
 		}
